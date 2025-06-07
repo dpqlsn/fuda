62d5d5be2c45cb309c74b91883758ae1ee0d2a3f
dpqlsn <a01037111209@gmail.com>
2025-06-07 21:13:27 +0900

    fix (#3) :: 기능 부분 더 필터링

--- fuda/src/components/Tag.tsx
+++ fuda/src/components/Tag.tsx
@@ -5,20 +5,20 @@ const TagContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-wrap: wrap;
     gap: 20px;
 `;
 
 const MainTag = styled.div`
-    width: 91px;
-    height: 40px;
+    padding: 8px 16px;
     font-size: 24px;
     background-color: #7BC357;
     color: #fff;
-    display: flex;
-    flex-direction: column;
+    display: inline-flex;
     align-items: center;
     justify-content: center;
     border-radius: 200px;
+    white-space: nowrap;
     cursor: pointer;
 
     :hover {
@@ -31,7 +31,10 @@ export default function Tag() {
         <TagContainer>
             <MainTag>#전체</MainTag>
             <MainTag>#인성</MainTag>
-            <MainTag>#기술</MainTag>
+            <MainTag>#백엔드</MainTag>
+            <MainTag>#CS</MainTag>
+            <MainTag>#프론트엔드</MainTag>
+            <MainTag>#프레임워크</MainTag>
         </TagContainer>
     );
 }
