8bdb3aa874e62a953c90aae776efe34872d883dc
dpqlsn <a01037111209@gmail.com>
2025-06-12 19:25:22 +0900

    feat (#15) :: 라우터 재설정

--- fuda/src/App.tsx
+++ fuda/src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './page/Landing';
 import Choice from './page/Main';
-import Entire from './page/Entire'
+import Entire from './page/Entire';
 import Save from './page/Save';
 
 export default function App() {
     return (
-        <Router>
+        <BrowserRouter>
         <Routes>
             <Route path="/" element={<Landing />} />
             <Route path="/choice" element={<Choice />} />
             <Route path="/entire" element={<Entire />} />
             <Route path="/save" element={<Save />} />
         </Routes>
-        </Router>
+        </BrowserRouter>
     );
 }
