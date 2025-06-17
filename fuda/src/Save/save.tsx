abf1573d530bee3150b09f44b5a17ef36312f5e3
dpqlsn <a01037111209@gmail.com>
2025-06-17 22:56:01 +0900

    feat (#18) :: 화면 pdf 변환

--- fuda/src/Save/save.tsx
+++ fuda/src/Save/save.tsx
@@ -3,7 +3,8 @@ import Bar from '../components/Navbar';
 import DownIcon from '../assets/download.svg';
 import '../App.css';
 import * as _ from './style';
-
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 interface Saved {
     question: string;
     answer: string;
@@ -22,21 +23,54 @@ export default function SavedList() {
         localStorage.setItem('savedQA', JSON.stringify(newList));
     };
 
+    const handleDownloadPDF = async () => {
+        const element = document.querySelector(".aiReport_page");
+
+        if (element !== null && element instanceof HTMLElement) {
+            const canvas = await html2canvas(element);
+            const imgData = canvas.toDataURL('image/png');
+
+            const pdf = new jsPDF('p', 'mm', 'a4');
+            const pageWidth = pdf.internal.pageSize.getWidth();
+            const pageHeight = pdf.internal.pageSize.getHeight();
+
+            const imgProps = pdf.getImageProperties(imgData);
+            const imgWidth = pageWidth;
+            const imgHeight = (imgProps.height * imgWidth) / imgProps.width;
+
+            let position = 0;
+
+            if (imgHeight > pageHeight) {
+                let heightLeft = imgHeight;
+                while (heightLeft > 0) {
+                    pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+                    heightLeft -= pageHeight;
+                    position -= pageHeight;
+                    if (heightLeft > 0) pdf.addPage();
+                }
+            } else {
+                pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+            }
+
+            pdf.save('savedQA.pdf');
+        }
+    };
+
     return (
         <>
             <Bar />
             <_.Container>
-                <_.MainArea>
+                <_.MainArea className="aiReport_page">
                     <_.TitleArea>
                         <_.Title>저장 된 질문보기</_.Title>
-                            <_.IconButton onClick={() => console.log('체크')}>
+                        <_.IconButton onClick={handleDownloadPDF}>
                             <img src={DownIcon} alt="Download" />
                         </_.IconButton>
                     </_.TitleArea>
                     {qaList.map((item: Saved, index: number) => (
                         <_.Item key={index}>
-                                <_.Question>{item.question}</_.Question>
-                                <_.Answer>{item.answer}</_.Answer>
+                            <_.Question>{item.question}</_.Question>
+                            <_.Answer>{item.answer}</_.Answer>
                             <_.DeleteButton onClick={() => handleDelete(index)}>삭제</_.DeleteButton>
                         </_.Item>
                     ))}
